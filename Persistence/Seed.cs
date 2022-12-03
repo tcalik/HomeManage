ea3c54d77a62e653903ed7a9de0d1b8fb102be66
tcalik <tcalik@protonmail.com>
2022-12-03 17:07:26 +0100

    Add project files.

---
+++ Persistence/Seed.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Rooms.Any()) return;
+
+            var rooms = new List<Room>
+            {
+                new Room {Name = "Living Room", Owner = "John"},
+                new Room {Name = "Kitchen", Owner = "John"},
+                new Room {Name = "Kitchen", Owner = "Eva"}
+            };
+            await context.Rooms.AddRangeAsync(rooms);
+            await context.SaveChangesAsync();   
+        }
+    }
+}
