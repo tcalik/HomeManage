facd77379155d6e74a990947c7c257ab6f9f895e
tcalik <tcalik@protonmail.com>
2022-12-19 16:41:47 +0100

    Cors setup, Basic login component

--- API/Program.cs
+++ API/Program.cs
@@ -37,6 +37,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt=>{
+    opt.AddPolicy("CorsPolicy", policy=>{
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:5173");
+    });
+});
+
 builder.Services.AddIdentityCore<AppUser>(opt =>
 {
     opt.Password.RequireNonAlphanumeric = false;
@@ -81,8 +87,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
+
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 
---
+++ client/src/components/Login/Login.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const Login: React.FC = () => {
+
+  const [login, setLogin] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:5296/api/account/login", {
+        email: login,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      <div className="Login grid grid-cols1 gap-2 w-48">
+        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1">Login</label>
+        <input onChange={e => setLogin(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red rounded py-2 px-2 mb-1"></input>
+        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1">Password</label>
+        <input onChange={e => setPassword(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-2 mb-2" type="password"></input>
+        <button onClick={handleSubmit} className="appearance-none block w-full bg-gray-400 text-gray-700 border border-gray-200 rounded py-2 px-2 mb-1">Submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
