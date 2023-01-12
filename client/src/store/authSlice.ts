d530ff2b4ca9a80c0582ff7513263bfedb17ce92
tcalik <tcalik@protonmail.com>
2023-01-12 01:15:02 +0100

    Rejected login handling in auth rtk slice

--- client/src/store/authSlice.ts
+++ client/src/store/authSlice.ts
@@ -4,42 +4,45 @@ import authService from "../services/auth.service";
 
 export const login = createAsyncThunk(
   "auth/login",
- async ({email, password}:{email: string, password:string}, thunkAPI) => {
-  try{
-    const data = await authService.login(email,password);
-    return {user: data};
+  async (
+    { email, password }: { email: string; password: string },
+    thunkAPI
+  ) => {
+    try {
+      const data = await authService.login(email, password);
+      return { user: data };
+    } catch (err) {
+      console.log(err);
+      return thunkAPI.rejectWithValue("Unable to login");
+    }
   }
-  catch(err){
-    console.log(err);
-  }
- }
-)
+);
 
 export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 });
 
-const initialState = {isLoggedIn: false, user: null}
+const initialState = { isLoggedIn: false, user: null };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers:{},
+  reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(login.fulfilled, (state, {payload})=>{
+    builder.addCase(login.fulfilled, (state, { payload }) => {
       state.isLoggedIn = true;
       state.user = payload!.user!;
-    })
+    });
     builder.addCase(login.rejected, (state, action) => {
       state.isLoggedIn = false;
       state.user = null;
-    })
+    });
     builder.addCase(logout.fulfilled, (state, action) => {
       state.isLoggedIn = false;
       state.user = null;
-    })
-  }
-})
+    });
+  },
+});
 
-const { reducer } = authSlice
+const { reducer } = authSlice;
 export default reducer;
